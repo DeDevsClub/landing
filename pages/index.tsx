159db59dad100826cec77cb87877633b079099c6
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-02-04 06:26:10 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type {NextPage} from 'next';
-import {Nav} from '../components/navbar/navbar';
+// import {Nav} from '../components/navbar/navbar';
 import {Layout} from '../components/navbar/layout';
 import {Hero} from '../components/hero';
 import {Box} from '../components/styles/box';
@@ -14,7 +14,7 @@ import {Trial} from '../components/trial';
 const Home: NextPage = () => {
    return (
       <Layout>
-         <Nav />
+{/* <Nav /> */}
          <Box as="main">
             <Hero />
             <Features3 />
